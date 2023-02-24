95c5f8f8cf0349b7e2d2efdcb434d5a5a8331e55
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-24 12:04:51 -0300

    casa

--- eventos.js
+++ eventos.js
@@ -1,4 +1,11 @@
-function selecionar() {
+function selecionar(input) {
     document.getElementById('quantidade').innerHTML = "1";
-    
+    if (input.checked){
+        quantidade.innerHTML = "1";
+
+    }else {
+        quantidade.innerHTML = "0";
+    }
+
+    input.checked ? quantidade.innerHTML = "1" : quantidade.innerHTML = "0";
 }
